eabe5371eb449b937cb85ff5ce6526725693cc15
ARParizi <52738836+ARParizi@users.noreply.github.com>
2025-03-01 22:32:05 +0330

    Start over once found a match

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,12 +1,15 @@
 const removeFromArray = function(arr, ...deleteValue) {
     const ans = [];
     console.log(deleteValue);
-    for (let i = 0; i < arr.length; i++)
+    L1: for (let i = 0; i < arr.length; i++)
     {
         ans.push(arr[i]);
         for (let j = 0; j < deleteValue.length; j++)
             if (arr[i] === deleteValue[j])
+            {
                 ans.pop();
+                continue L1;
+            }
     }
     return ans;
 };
