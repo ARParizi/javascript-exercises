41603b2f409ebd0af04932fe05227da6403af539
ARParizi <52738836+ARParizi@users.noreply.github.com>
2025-03-01 23:03:15 +0330

    Solve exercise, pass all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(numA, numB) {
+    if(!Number.isInteger(numA) || !Number.isInteger(numB) || numA < 0 || numB < 0)
+        return 'ERROR';
+    let ans  = 0;
+    let step = 1;
+    if(numA > numB)
+        step = -1;
+    for (let ii = numA; ii != numB + step; ii += step) {
+        ans += ii
+    }
+    return ans;
 };
 
 // Do not edit below this line
