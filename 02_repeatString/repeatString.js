96500b6c2aef34bd83aa677bc5cf4f4ac6d77365
ARParizi <52738836+ARParizi@users.noreply.github.com>
2025-03-01 20:12:59 +0330

    Solve exercise, 7/8 tests shall pass

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let ans = '';
+    for (let i = 0; i < num; i++) {
+        ans += string;
+    }
+    return ans;
 };
 
 // Do not edit below this line
