93e7b9bfc19a5f83fd0b23335897b4af3e708c35
ARParizi <52738836+ARParizi@users.noreply.github.com>
2025-03-01 22:09:41 +0330

    Pass the first test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, deleteValue) {
+    const ans = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] != deleteValue)
+            ans.push(arr[i]);
+    }
+    return ans;
 };
 
 // Do not edit below this line
